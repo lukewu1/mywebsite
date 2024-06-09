8623ec14c57197f8cf312e3dadd5bc4d3188b4a1
Luke Wu <lukewu@Lukes-MacBook-Pro-2.local>
2024-06-08 19:58:17 -0600

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+// import Experience from './pages/Experience'
+// import Projects from './pages/Projects'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<About />} />
+          <Route path="/" element={<About />} />
+          {/* <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+// import Header from './Header'
+// import Footer from './Footer'
+import profilePicture from '../downloads/IMG_4875.jpg'
+
+export default function About(){
+    return(
+        <div>
+            <Header />
+            <div className="body">
+                <div className="text">
+                    <h1>Luke Wu</h1>
+                    <p>
+                        I am currently a Junior at the University of Colorado Boulder studying for a Bachelor of Science in Computer Science 
+                        and a Minor in Data Science. I am passionate about software development, artificial intelligence, and data analytics. <br /> <br />
+                        Outside of school, I enjoy rock climbing, weightlifting, tennis, and pickleball. <br /> <br />
+                        Notable Achievements: Deans list(2023-current), <a href="https://devpost.com/software/mega-problem-solver" target="_blank">HackCU Winner (Games)</a>
+                    </p>
+                </div>
+                <div className="profile">
+                    <img src={profilePicture} class="profile"/>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
