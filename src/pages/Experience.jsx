3db4d84c932af7e29c110b84c78e4bb3bcfcd40a
Luke Wu <lxwu24@gmail.com>
2024-09-03 17:16:13 -0600

    new updates

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,14 +1,31 @@
 import Header from './Header'
 import Footer from './Footer'
 import LA from '../downloads/download (3).png'
+import NH from '../downloads/Screenshot 2024-09-03 at 5.05.09 PM.png'
 
 export default function Experience(){
     return(
         <>
             <Header />
+            <div className="experiencetitle">
+                <h1>Experience</h1>
+            </div>
             <div className="body">
                 <div className="text">
-                    <h1>Experience</h1>
+                    <p>
+                        Contributed to the backend development of a full-stack CRUD application aimed at providing healthy alternatives to drugs, alcohol, and other unhealthy coping mechanisms, primarily for teens. The application was built using Svelte for the frontend and Firebase for the backend, offering a user-friendly platform to support the organization's mission.
+                    </p>
+                    <br></br>
+                    <p>
+                        Tools & Technologies: Svelte, Firebase, Full-Stack Development.
+                    </p>
+                </div>
+                <div className="profile">
+                    <img src={NH} className="la"/>
+                </div>
+            </div>
+            <div className="body">
+                <div className="text">
                     <p>
                     I have mentored over 200 students in CU's Intro to Computer Science course, 
                     facilitating their learning through weekly recitations and providing additional support during office hours. <br /> <br />
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer'
 import ML from '../downloads/ss.png'
 import semantle from '../downloads/cover.png'
 import IMDB from '../downloads/IMDb-logo.png'
+import AI from '../downloads/Site.png'
 
 export default function Projects(){
     return(
@@ -15,19 +16,28 @@ export default function Projects(){
                 <div className="body">
                     <div className="text">
                         <p>
+                        <a href="https://github.com/lukewu1/AI-PDF-Summarizer" target="_blank">AI PDF Summarizer</a> <br />
+                        Description: Integrated Mr2Wei's AI PDF Summarizer with the Notion API, allowing users to automatically send summaries to their Notion pages. Developed a secure backend that requires users to run the code locally to enable the Notion integration, ensuring both data protection and user control.<br /> <br />
+                        Tools used: JavaScript, HTML, CSS, Notion API, Backend Development
+                        </p>
+                    </div>
+                    <div className="profile">
+                        <img src={AI} className="la"/>
+                    </div>
+                </div>
+                <div className="body">
+                    <div className="profile">
+                        <img src={ML} className="la"/>
+                    </div>
+                    <div className="text">
+                        <p>
                         <a href="https://github.com/lukewu1/Machine-Learning-Project" target="_blank">Machine Learning Project</a> <br />
                         Description: Developed multiple machine learning models using multiple linear regression to predict housing prices.<br /> <br />
                         Tools used: Python, Pandas, Numpy, Matplotlib, Seaborn, Sklearn, Plotly.express
                         </p>
                     </div>
-                    <div className="profile">
-                        <img src={ML} className="la"/>
-                    </div>
                 </div>
                 <div className="body">
-                    <div className="profile">
-                        <img src={semantle} className="la"/>
-                    </div>
                     <div className="text">
                         <p>
                         <a href="https://github.com/lukewu1/Semantle-AI-Solver" target="_blank">Semantle AI Solver</a> <br />
@@ -35,17 +45,20 @@ export default function Projects(){
                         Tools used: Python, Numpy, Linear Algebra, Word2Vec, RESTful API
                         </p>
                     </div>
+                    <div className="profile">
+                        <img src={semantle} className="la"/>
+                    </div>
                 </div>
                 <div className="body">
+                    <div className="profile">
+                        <img src={IMDB} className="la"/>
+                    </div>
                     <div className="text">
                         <p>
                         <a href="https://github.com/lukewu1/IMDB-Top-1000-Database" target="_blank">IMDB Top 1000</a> <br />
                         Designed a C++ program to store movie details in a hash table and efficiently search through a skip list. <br /> <br />
                         Tools used: C++, Skip Lists, Hash Tables
                         </p>
-                    </div>
-                    <div className="profile">
-                        <img src={IMDB} className="la"/>
                     </div>
                 </div>
             </div>
